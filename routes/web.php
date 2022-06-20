bdeb7e9954fd40160ecc6a041c372c21f2659670
Lương Quang Pháp <phapbro233@gmail.com>
2022-06-21 01:28:11 +0700

    Setup Admin Dashboard and Grouping Routes for admin

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AdminController\DashboardController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\TagController;
 use App\Models\Post;
 
+
+//Front User Routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostsController::class, 'show'])->name('posts.show');
@@ -37,3 +40,10 @@ Route::get('/categories', [CategoryController::class, 'index'])->name('categorie
 Route::get('/tags/{tag:name}', [TagController::class, 'show'])->name('tags.show');
 
 require __DIR__ . '/auth.php';
+
+
+
+//Admin Dashboard Routes
+Route::prefix('admin')->name('admin.')->middleware('auth', 'isadmin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+});
